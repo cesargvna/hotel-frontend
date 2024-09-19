5af282117f51ebb70cfcff423b57625f05aa41bb
cesargvna <cesargvna@gmail.com>
2024-09-19 12:12:36 -0400

    items sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,7 +4,29 @@ import HotelIcon from '@mui/icons-material/Hotel';
 import RoomPreferencesIcon from '@mui/icons-material/RoomPreferences';
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
 import HorizontalSplitIcon from '@mui/icons-material/HorizontalSplit';
+import styled from "styled-components";
 //import "./styles/Sidebar.css";
+
+const MainContainer = styled.div`
+  display: flex;
+`;
+const SidebarContent = styled.div`
+  width: ${({ isOpen }) => isOpen ? "240px" : "50px"};
+  height: 92vh;
+  background-color: #1868b8;
+  padding:10px;
+`;
+
+const SidebarItem = styled(NavLink)`
+  display:flex;
+  gap:10px;
+  text-decoration:none;
+  font-size:20px;
+  color:#fff;
+`;
+const ItemName = styled.div`
+  display:${({ isOpen }) => isOpen ? "block" : "none"};
+`;
 
 const Sidebar = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -26,35 +48,30 @@ const Sidebar = ({ children }) => {
       icon: <SupportAgentIcon />,
     },
   ];
-  return (
-    <div className="sidebar-container">
-      <div style={{ width: isOpen ? "260px" : "50px" }} className="sidebar">
-        <div className="top_section">
-          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
-            <img style={{ height: "40px" }} src="dfs" alt='logo' />
-          </h1>
-          <div className="container-bars">
-            <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars ">
-              <HorizontalSplitIcon style={{ color: "#000000", }} onClick={toggle} />
-            </div>
-          </div>
 
+  return (
+    <MainContainer>
+      <SidebarContent isOpen={isOpen}>
+        <div className="top_section">
+          <HorizontalSplitIcon style={{ color: "#000000", }} onClick={toggle} />
         </div>
         {menuItem.map((item, index) => (
-          <NavLink to={item.path} key={index} className="link" activeclassname="active">
-            <div className="icon">{item.icon}</div>
-            <div style={{ display: isOpen ? "block" : "none" }} className="link_text">
-              {item.name}
+          <SidebarItem >
+            <div style={{ fontSize: "40px" }}>
+              {item.icon}
             </div>
-          </NavLink>
-        ))}
-      </div>
+            <ItemName isOpen={isOpen}>
+              {item.name}
+            </ItemName>
+          </SidebarItem>
+        ))
+        }
+      </SidebarContent >
+
       <main>
         {children}
       </main>
-    </div>
-
-
+    </MainContainer >
   );
 };
 
