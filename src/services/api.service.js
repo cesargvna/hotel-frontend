57e334b9dd27720c1d9c8d00cda8f2615df6ff48
cesargvna <cesargvna@gmail.com>
2024-10-07 21:47:11 -0400

    fix url

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -3,27 +3,27 @@ import FormData from "form-data";
 import { getFromLocalStorage } from "../utilities/local-storage-manager";
 
 //const API_URL = "http://localhost:3000/api";
-const API_URL = "/api";
+const baseUrl = "/api";
 
 const login = async (email, password) => {
-  return await axios.post(`${API_URL}/login`, { email, password });
+  return await axios.post(`${baseUrl}/login`, { email, password });
 };
 const register = async (email, password) => {
-  return await axios.post(`${API_URL}/register`, { email, password });
+  return await axios.post(`${baseUrl}/register`, { email, password });
 };
 
 const API_SERVICE = {
   get: async (url) => {
-    return await axios.get(`${API_URL + url}`);
+    return await axios.get(`${baseUrl + url}`);
   },
   post: async (url, data) => {
-    return await axios.post(`${API_URL + url}`, data);
+    return await axios.post(`${baseUrl + url}`, data);
   },
   put: async (url, data) => {
-    return await axios.put(`${API_URL + url}`, data);
+    return await axios.put(`${baseUrl + url}`, data);
   },
   delete: async (url) => {
-    return await axios.delete(`${API_URL + url}`);
+    return await axios.delete(`${baseUrl + url}`);
   },
 };
 const POST_IMAGE = async (url, data) => {
@@ -37,7 +37,7 @@ const POST_IMAGE = async (url, data) => {
   formData.append("image", data.file); // Agrega la imagen al FormData
 
   console.log([...formData]); // Muestra el contenido de formData
-  const response = await fetch(`${API_URL + url}`, {
+  const response = await fetch(`${baseUrl + url}`, {
     method: "POST",
     body: formData,
     headers: {
